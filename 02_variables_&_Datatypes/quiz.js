153dee92d480a5646cc31dddf1a4a478ffed8f1a
Manas <manassoni1311@gmail.com>
2025-07-09 16:07:01 +0530

    quiz added

---
+++ 02_variables_&_Datatypes/quiz.js
@@ -0,0 +1,18 @@
+/* Quiz for undestanding better 
+
+Question 1 -> create variable of string and try to add number to it and observe 
+Question 2 -> find type of ans of previos question 
+Question 3 -> make a english to hindi word meaning dictionary 
+
+*/
+
+console.log('Solution of first question ');
+
+let a = "manas";
+
+a = a + 1;
+
+console.log(a); // observation is if we add number in string so updated one is also a string 
+
+console.log('Solution of second question');
+console.log("type of a is :",typeof(a));
